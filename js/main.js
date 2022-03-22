2dca89041db6a7211a4a4feb9af108e8aa61697f
imronnigmatov <imronnigmatov@gmail.com>
2022-03-23 01:37:42 +0500

    js teach

---
+++ js/main.js
@@ -0,0 +1,57 @@
+//Number
+
+let a = 5;
+let b = 6;
+let variables = 330
+const sum = a + b;
+console.log(sum + variables)
+
+const aka = 9;
+const Bratan = aka;
+console.log(Bratan)
+
+let c = "Hormela okala"
+let d = "Salomat bol ukajonim"
+let e = c + d
+console.log(e = c + d)
+
+//String
+/* const Text = "Assalomu alekum"
+console.log(Text)
+const text = "Valekum Assalom "
+console.log(text) */
+
+//STRING
+/* const tex = "Bratan";
+let number = " Nma gap";
+console.log(tex + number) */
+
+//String
+/* const f = 1;
+const g = 2;
+const h = "5";
+console.log(( g + f) + h)
+//STRING
+const i = 1;
+const j = 5;
+const k = "5"
+console.log( i + j + k)
+//Stringni rangi tuzilishi
+const l = "9"
+console.log(i) */
+
+/* const UserTxt = Number(prompt("Yoshingizni kiriting !"))
+alert (UserTxt)
+console.log (UserTxt)
+
+const userText = (prompt("Ismingizni kiriting"))
+alert (userText)
+console.log (userText) */
+
+//Vazifa sinf iwi
+
+const UserTxt = Number(prompt("1 chi sonni kiriting"))
+
+const userText = Number(prompt("2 chi sonni kiriting"))
+
+alert("Sizning Jovobingiz " + (UserTxt + userText) + " boladi deb tog`ri oylayman")
